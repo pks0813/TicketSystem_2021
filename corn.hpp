5f7d42d000c3c53fe7b858b70ebc20c3e17c1475
pks0813 <2508697539@qq.com>
2021-05-25 00:58:21 +0800

    little

--- corn.hpp
+++ corn.hpp
@@ -432,9 +432,9 @@ class Corn{
         int Sday=StrToDate(tmp[6]);
         sjtu::vector<int> STrainVec=StationTrainBPT.Findinterval(std::make_pair(SStationkey,-1),std::make_pair(SStationkey,1ll<<62));
         sjtu::vector<int> TTrainVec=StationTrainBPT.Findinterval(std::make_pair(TStationkey,-1),std::make_pair(TStationkey,1ll<<62));
-        TrainInfo TrainInfoTvec[2000];
+        TrainInfo TrainInfoTvec[5000];
         int Tvecsize=TTrainVec.size();
-        if (Tvecsize>2000)
+        if (Tvecsize>5000)
         {
             printf("666");
             exit(0);
