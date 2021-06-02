1495a6f07d31674113e20a3083148419ea3654db
pks0813 <2508697539@qq.com>
2021-06-02 16:47:32 +0800

    ???

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 int sumCheck;
 Corn::TrainInfo TrainInfoTvec[6000];   
 int main(){
-    hc(sizeof(Corn::User));
-    hc(sizeof(Corn::TrainInfo));
-    hc(sizeof(Corn::Order));
-    hc(sizeof(Corn::Seat));
+    // hc(sizeof(Corn::User));
+    // hc(sizeof(Corn::TrainInfo));
+    // hc(sizeof(Corn::Order));
+    // hc(sizeof(Corn::Seat));
     {
         Corn pks;
         pks.game();
